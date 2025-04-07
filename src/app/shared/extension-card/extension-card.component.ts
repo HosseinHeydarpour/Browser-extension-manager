088a11fe698feb2ad8f8b21b217d313f08e240bd
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-07 23:37:06 +0330

    feat: add extension card component

---
+++ src/app/shared/extension-card/extension-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extension-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './extension-card.component.html',
+  styleUrl: './extension-card.component.scss'
+})
+export class ExtensionCardComponent {
+
+}
