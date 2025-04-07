9a9a3111d60cc3d2756aa3c6e0e79cf4b6a82e31
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-07 23:37:45 +0330

    feat: add extension list component

---
+++ src/app/features/extension-list/extension-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extension-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './extension-list.component.html',
+  styleUrl: './extension-list.component.scss'
+})
+export class ExtensionListComponent {
+
+}
